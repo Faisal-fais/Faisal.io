fe6d43ec9e2ad5aa2617f1be6398e87cec60f558
faisal-fais <faizalmho@gmail.com>
2025-03-14 21:41:29 +0800

    Paymen gateway

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Toggle class active
 const navbarNav = document.querySelector('.navbar-nav');
-// ketika hamburger menu diklik
 document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
     e.preventDefault();
